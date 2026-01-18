a0f61cb5e87ef4b637cbd21d27021871f35c91b3
7Majesty-M <thebestmajesty@gmail.com>
2026-01-19 00:51:53 +0500

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { SessionProvider } from "next-auth/react"
 import './globals.css'
+export const dynamic = 'force-dynamic'; 
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
