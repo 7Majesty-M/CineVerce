902b5427e32bcd892dd42bac0549e7098c63d1ad
7Majesty-M <thebestmajesty@gmail.com>
2026-01-25 00:02:42 +0500

    fix build

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,44 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  
+  // Добавляем управление кэшем
+  async headers() {
+    return [
+      {
+        // 1. Правило для всех страниц сайта: ЗАПРЕТИТЬ КЭШ
+        // Это решит проблему, когда у друга открывается старая версия
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+      {
+        // 2. Правило для системных файлов Next.js (скрипты, стили): РАЗРЕШИТЬ КЭШ
+        // Это нужно, чтобы сайт грузился быстро и не скачивал движок React каждый раз заново
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        // 3. Правило для картинок Next.js: РАЗРЕШИТЬ КЭШ
+        source: '/_next/image/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
